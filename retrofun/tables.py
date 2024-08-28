ae7508870bc50038f75b334ab5120fe00ba6c36c
Sanghyuk Park <sanghyuk.park85@gmail.com>
2024-08-28 14:03:35 -0500

    create manufacturers table with foreignkey from products to manufacturers

--- retrofun/tables.py
+++ retrofun/tables.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Table, Column, Integer, String
+from sqlalchemy import Table, Column, Integer, String, ForeignKey
 from retrofun.database import metadata
 
 Product = Table(
@@ -6,6 +6,15 @@ Product = Table(
     metadata,
     Column('id', Integer, primary_key=True),
     Column('name', String(64), unique=True, index=True, nullable=False),
+    Column('manufacturer_id', Integer, ForeignKey('manufacturers.id'), nullable=False),
     Column('year', Integer, index=True, nullable=False),
-    Column('cpu', String(32), nullable=True)
+    Column('cpu', String(32), nullable=True),
 )
+
+Manufacturer = Table(
+    'manufacturers',
+    metadata,
+    Column('id', Integer, primary_key=True),
+    Column('name', String(64), unique=True, nullable=False, index=True),
+)
+
